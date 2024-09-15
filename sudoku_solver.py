2219bbd3748c3adae65096ae7264cce782d18666
Sri Bharath Eshwar <bharatheshwar95@gmail.com>
2024-09-15 13:50:28 +0530

    Create sudoku_solver.py
    This repository contains a Python implementation of a Sudoku solver using a backtracking algorithm. The program takes an input Sudoku grid, where empty cells are represented by zeros, and automatically fills in the missing numbers to complete the puzzle.

---
+++ sudoku_solver.py
@@ -0,0 +1,83 @@
+def print_board(board):
+    """Print the Sudoku board in a readable format."""
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def find_empty(board):
+    """Find an empty space (represented by 0) on the board."""
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)  # row, col
+    return None
+
+def is_valid(board, num, pos):
+    """Check whether placing a number in a specific position is valid."""
+    # Check the row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check the column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check the 3x3 grid
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+def solve(board):
+    """Solve the Sudoku puzzle using backtracking."""
+    empty = find_empty(board)
+    if not empty:
+        return True  # Puzzle solved
+    else:
+        row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, num, (row, col)):
+            board[row][col] = num
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0  # Backtrack
+
+    return False
+
+# Example Sudoku puzzle (0 represents empty spaces)
+board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+]
+
+print("Sudoku Puzzle (Unsolved):")
+print_board(board)
+solve(board)
+print("\nSudoku Puzzle (Solved):")
+print_board(board)
